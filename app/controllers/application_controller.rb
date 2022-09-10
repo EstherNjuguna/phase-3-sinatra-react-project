52fd8d48860cb3703aa893a2b6faff7eafa7ad6c
+ <+>
2022-09-10 09:46:16 +0300

    controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,84 @@
+
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+   get '/' do
+   "Hello World"
+  end 
+ 
+  get '/ingredients' do
+    ingredients = Ingredient.all
+    ingredients.to_json
+  end
+  
+  
+  post "/ingredients" do
+    ingredients = Ingredient.create(
+      name: params[:name],
+      ingred_id: params[:ingred_id]
+    )
+    ingredients.to_json
   end
 
+  delete "/ingredients/:id" do
+    ingredient = Ingredient.find(params[:id])
+    ingredient.destroy
 end
+
+
+  
+  get '/categories' do
+    categories = Category.all
+    categories.to_json include: :ingredients
+  end
+
+  get "/categories/:id" do
+    category = Category.find_by_id(params[:id])
+    category.to_json include: :ingredients
+  end
+  
+  post "/categories" do
+    category = Category.create(
+      name: params[:name],
+      procedure: params[:procedure],
+      price: params[:price]
+    )
+    category.to_json
+  end
+
+  delete "/categories/:id" do
+    category = Category.find(params[:id])
+    category.destroy
+    Category.all.to_json
+  end
+
+
+
+ get "/recipes" do
+    recipe = Recipe.all
+    recipe.to_json
+  end
+
+get "/recipes/:id" do
+    recipe = Recipe.find_by_id(params[:id])
+    recipe.to_json
+end
+
+post "/recipes" do
+    recipe = Recipe.create(
+      name: params[:name],
+      instructions: params[:instructions],
+      procedure: params[:procedure],
+      meal_id: params[:meal_id]
+    )
+    recipe.to_json
+  end
+
+delete "/recipes/:id" do
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+end
+end
+  
